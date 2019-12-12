e51c51b46dff687ea70378bdb3969931c1c6e621
Doo Nam Kim <doonam.kim@pnnl.gov>
2019-12-12 15:14:37 -0800

    minor

--- run_me.py
+++ run_me.py
@@ -33,12 +33,12 @@ def run_BLAST_by_NCBI(input_fasta_name):
 if (__name__ == "__main__") :
     args=sys.argv[1:]
     if len(args) < 1:
-        print "input format: python auto_homology.py <fasta file that NCBI BLAST will be ran against> \n"
-        print "example usage: python auto_homology.py DGAT_target.fasta \n"
+        print "input format: python run_me.py <a fasta file name that NCBI BLAST will be ran against> \n"
+        print "example usage: python run_me.py DGAT_target.fasta \n"
         exit(1)
 
 fasta_file_name = args[0]
-print "Step 1. Auto_homology searches NCBI website with " + str(fasta_file_name) + ". Please wait.\n"
+print "Step 1. AutoHomology searches NCBI website with " + str(fasta_file_name) + ". Please wait.\n"
 print "\tFor example,\n"
 print "\t41 amino acids tend to take 1 minute.\n"
 print "\t451 amino acids tend to take 2 minutes.\n"
