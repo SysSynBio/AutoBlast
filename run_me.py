b21e257f155748ee0d94b69916ea8665e73af598
Doo Nam Kim <doonam.kim@pnnl.gov>
2019-12-13 11:07:42 -0800

    now read all retrievals

--- run_me.py
+++ run_me.py
@@ -9,40 +9,46 @@ util_path = current_dir + "/util/"
 sys.path.insert(0, util_path)
 from util import *
 
-def analyze_BLAST_result(result_handle):
+def analyze_BLAST_result(input_fasta_name_wo_path, result_handle):
     print "\nStep 2. Analyze the BLAST result."
     blast_records = NCBIXML.parse(result_handle)
+    '''
+    output_file_name = "retrieved_from_" + str(input_fasta_name_wo_path)[:-6] + ".xml"
     
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    current_dir = os.getcwd()    
+    output_folder = os.path.join(current_dir, "output")
+    
+    os.chdir(output_folder)
+    
+    output_file = open(output_file_name, "w") # since it is 'w', an existing file will be overwritten. (if this is "a", new info will be appended to an existing file)
+    output_file.write(result_handle.read())
+    output_file.close()
+    '''
     blast_records_list = list(blast_records) # once blast_records becomes a list, blast_records is no longer accessible
-    print "len(blast_records_list): ", len(blast_records_list)
     
-    #E_VALUE_THRESH = 0.04
-    
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
+    for blast_record_list in blast_records_list:
+        for alignment in blast_record_list.alignments:
             for hsp in alignment.hsps:
                 print ("****Alignment****")
                 print ("sequence:", alignment.title)
                 print ("length:", alignment.length)
                 print ("e value:", hsp.expect)
-                print (hsp.query[0:75] + "...")
-                print (hsp.match[0:75] + "...")
-                print (hsp.sbjct[0:75] + "...")
-    
-    '''
-    output_file = open(output_file_name, "w") # since it is 'w', an existing file will be overwritten. (if this is "a", new info will be appended to an existing file)
-    output_file.write(result_handle.read())
-    output_file.close()
-    '''
+                #print (hsp.query[0:75] + "...")
+                #print (hsp.match[0:75] + "...")
+                #print (hsp.sbjct[0:75] + "...")
     
 ######## end of def analyze_BLAST_result(blast_records)
 
 
 def run_BLAST_by_NCBI(input_fasta_name):
-    print "\nStep 1. AutoHomology is searching NCBI website (blastp) with " + str(fasta_file_name) + ". Please wait."
+    print "\nStep 1. AutoHomology is searching NCBI website (blastp) with " + str(fasta_file_name)
+    print "\tPlease wait."
     print "\tAs references,"
-    print "\t\t41 amino acids tend to take 1 minute."
-    print "\t\t451 amino acids tend to take 2 minutes."
+    print "\t\t 41 amino acids take ~1 minute."
+    print "\t\t451 amino acids take ~2 minutes."
 
     record = SeqIO.read(input_fasta_name, format="fasta")
     
@@ -55,18 +61,8 @@ def run_BLAST_by_NCBI(input_fasta_name):
     print show_time("\tBLAST searching", time_start_of_searching, time_end_of_searching)
     
     input_fasta_name_wo_path = os.path.basename(input_fasta_name)
-    
-    output_file_name = "retrieved_from_" + str(input_fasta_name_wo_path)[:-6] + ".xml"
-    
-    if not os.path.exists('output'):
-        os.makedirs('output')
-
-    current_dir = os.getcwd()    
-    output_folder = os.path.join(current_dir, "output")
-    
-    os.chdir(output_folder)
                     
-    return result_handle
+    return input_fasta_name_wo_path, result_handle
 ######## end of def run_BLAST_by_NCBI(fasta_file_name)
     
 
@@ -79,7 +75,7 @@ if (__name__ == "__main__") :
 
 fasta_file_name = args[0]
 
-result_handle = run_BLAST_by_NCBI(fasta_file_name)
+input_fasta_name_wo_path, result_handle = run_BLAST_by_NCBI(fasta_file_name)
 #print "\tSee retrieved results at the output folder.\n"
 
-analyze_BLAST_result(result_handle)
+analyze_BLAST_result(input_fasta_name_wo_path, result_handle)
